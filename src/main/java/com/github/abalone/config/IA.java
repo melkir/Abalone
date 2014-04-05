f25871fbd747bb171e5dbcdeb630c9e86b171cbf
melkir <melkir13@gmail.com>
2014-04-05 22:18:30 +0200

    add IA conf

---
+++ src/main/java/com/github/abalone/config/IA.java
@@ -0,0 +1,36 @@
+package com.github.abalone.config;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by melkir on 05/04/14.
+ */
+public class IA extends ConstraintValue<String> {
+
+    private static HashSet<String> list;
+
+    public IA(String description, String value) {
+        super(description, value);
+    }
+
+    @Override
+    protected void initConstraint() {
+        if (IA.list != null) return;
+        IA.list = new HashSet<String>();
+        IA.list.add("MiniMax");
+        IA.list.add("AlphaBeta");
+        IA.list.add("NegaScout");
+    }
+
+    @Override
+    protected Boolean check(String value) {
+        return IA.list.contains(value);
+    }
+
+    @Override
+    public Set<String> getList() {
+        return Collections.unmodifiableSet(IA.list);
+    }
+}
